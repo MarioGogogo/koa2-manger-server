f3639a477fdffa0a969c5a5f8b41e764113db31a
mario <148373644@qq.com>
2021-09-22 22:44:18 +0800

    修复：二级审核人审核状态判断，二级以上审核人请求列表数据

--- server/routes/approve.js
+++ server/routes/approve.js
@@ -1,7 +1,7 @@
 /*
  * @Author: MarioGo
  * @Date: 2021-09-16 19:43:19
- * @LastEditTime: 2021-09-16 19:56:19
+ * @LastEditTime: 2021-09-22 22:06:02
  * @LastEditors: MarioGo
  * @Description: 文件描述
  * @FilePath: /manager-server/server/routes/approve.js
@@ -20,27 +20,43 @@ export class approveController {
     const { action, remark, _id } = ctx.request.body;
     let authorization = ctx.request.headers.authorization;
     let { data } = Utils.decoded(authorization);
-    let parmams = {};
+    let params = {};
     // 1 待审核 2 审批中 3 审批拒绝 4 审批通过 5 作废
-    let doc = Leave.findById(_id);
-    // 拒绝
-    if (action === 'refuse') {
-      params.applyState = 3;
-    } else {
-    }
-    auditLogs.push({
-      userId: data.userId,
-      userName: data.userName,
-      createTime: new Date(),
-      remark,
-      action: action === 'refuse' ? '审核拒绝' : '审核通过',
-    });
-    params.auditLogs = auditLogs;
     try {
-      let res = await Leave.findByIdAndUpdate(_id, parmas);
+      let doc = await Leave.findById(_id);
+
+      let auditLogs = doc.auditLogs || [];
+      // 拒绝
+      if (action === 'refuse') {
+        params.applyState = 3;
+      } else {
+        // 1.当审核长度已经满了说明 全部审核了
+        if(doc.auditFlows.length === doc.auditLogs.length){
+          ctx.body = Utils.success('当前申请单已处理，请勿重复提交');
+          return
+        }else if(doc.auditFlows.length === doc.auditLogs.length + 1){
+          //2.优先判断如果最后一级审批人  [A,B,C]
+          params.applyState = 4;
+        }else if(doc.auditFlows.length > doc.auditLogs.length){
+          //审核通过 当前审核人要前进一位；
+           params.applyState = 2;
+           params.curAuditUserName = doc.auditFlows[doc.auditLogs.length + 1].userName;
+        }
+        
+      }
+      auditLogs.push({
+        userId: data.userId,
+        userName: data.userName,
+        createTime: new Date(),
+        remark,
+        action: action === 'refuse' ? '审核拒绝' : '审核通过',
+      });
+      params.auditLogs = auditLogs;
+      let res = await Leave.findByIdAndUpdate(_id, params);
+      console.log('%c 🍎 res: ', 'font-size:20px;background-color: #FCA650;color:#fff;', res);
       ctx.body = Utils.success('', '处理成功');
     } catch (error) {
-      ctx.body = Utils.fail('查询失败');
+      ctx.body = Utils.fail(`查询失败:${error.message}`);
     }
   }
 }
--- server/routes/leave.js
+++ server/routes/leave.js
@@ -1,12 +1,14 @@
 /*
  * @Author: MarioGo
  * @Date: 2021-08-22 16:49:34
- * @LastEditTime: 2021-09-15 23:00:15
+ * @LastEditTime: 2021-09-22 22:11:11
  * @LastEditors: MarioGo
  * @Description: 休假申请接口
  * @FilePath: /manager-server/server/routes/leave.js
  * 可以输入预定的版权声明、个性签名、空行等
  */
+
+import { apply } from 'ramda';
 
 const { controller, get, post, put } = require('../lib/decorator');
 const Leave = require('../models/leaveSchema');
@@ -15,42 +17,58 @@ const Dept = require('../models/deptSchema');
 
 @controller('/api/leave')
 export class leaveController {
+  //获取审核消息列表 消息
   @get('/noticeCount')
   async getNoticeCount(ctx) {
-    ctx.body = Utils.success({});
+    let authorization = ctx.request.headers.authorization;
+    let { data } = Utils.decoded(authorization);
+    try {
+      let params = {};
+      params.curAuditUserName = data.userName;
+      params.$or = [{ applyState: 1 }, { applyState: 2 }];
+      const total = await Leave.countDocuments(params);
+      console.log('%c 🥦 total: ', 'font-size:20px;background-color: #2EAFB0;color:#fff;', total);
+      ctx.body = Utils.success(total);
+    } catch (error) {
+      ctx.body = Utils.fail(`查询失败:${error.stack}`);
+    }
   }
   // 查询申请列表
   @get('/leaveList')
   async getLeaveListt(ctx) {
-    const { applyState,type } = ctx.request.query;
+    const { applyState, type } = ctx.request.query;
     const { page, skipIndex } = Utils.pager(ctx.request.query);
     let authorization = ctx.request.headers.authorization;
     let { data } = Utils.decoded(authorization);
     try {
-      let params = {}
+      let params = {};
       //如果是审核人 则需要返回审核人 范围内的请假条码
-      if(type === "approval"){
+      if (type === 'approval') {
         //根据审核状态去查数据
-        if(applyState === 1){
+        if (Number(applyState) === 1 || Number(applyState) === 2) {
           //当前审核人 （当前人老板 如果经理还没审核通过 老板也看不见）
-           params.curAuditUserName = data.userName
-           params.applyState = 1
-        }else if(applyState > 1){
-             //非常重要的子文档查询 🔥🔥🔥🔥🔥🔥🔥
-             params = {"auditFlows.userId":data.userId,applyState}
-        }else{
-          params = {"auditFlows.userId":data.userId}
+          params.curAuditUserName = data.userName;
+          params.$or = [{ applyState: 1 }, { applyState: 2 }];
+          //  params.applyState = 1
+        } else if (Number(applyState) > 1) {
+          //非常重要的子文档查询 🔥🔥🔥🔥🔥🔥🔥
+          params = { 'auditFlows.userId': data.userId, applyState };
+        } else {
+          params = { 'auditFlows.userId': data.userId };
         }
-      }else{
-       params = {
+      } else {
+        params = {
           'applyUser.userId': data.userId,
         };
+        //传过来的是字符串 必须转 数字
+        if (Number(applyState)) params.applyState = applyState;
       }
-    
-      //传过来的是字符串 必须转 数字
-      if (Number(applyState)) params.applyState = applyState;
+      console.log(
+        '%c 🍖 params: ',
+        'font-size:20px;background-color: #42b983;color:#fff;',
+        params
+      );
       const query = Leave.find(params); //promise对象
-
       // 查找第几页后10条数据
       const list = await query.skip(skipIndex).limit(page.pageSize);
       //获取 某某 对应多少条
